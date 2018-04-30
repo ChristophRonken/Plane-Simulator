f4866d39f19f84fe13129720c23f9654547024cd
ChristophRonken <christoph.ronken@student.uantwerpen.be>
2018-05-01 00:31:51 +0200

    FlightPlan  name convention + documentation

--- src/FlightPlan.h
+++ src/FlightPlan.h
@@ -1,6 +1,6 @@
-//
-// Created by oliviervh on 12.04.18.
-//
+// author: Ronken Christoph & Van Hautte Olivier
+// date: 02/05/2018
+// version: 2.0
 
 #ifndef PSE_V1_AIRPLANEROUTE_H
 #define PSE_V1_AIRPLANEROUTE_H
